e742621ebd00abb650dd273bde511a0ef0a0f0dc
Matyáš Skalický <matyaskalicky@gmail.com>
2018-04-12 00:50:05 +0200

    Added comments

--- spravujklub/views.py
+++ spravujklub/views.py
@@ -9,10 +9,15 @@ def index():
     email = request.values.get("mail")
 
     if name and email:
-        print("Adding: " + name + " mail: " + email)
+        # Create a new member
         new_member = Member(name=name, email=email)
+        # Add it to the database session
         db.session.add(new_member)
+        # Commit the change to the DB
         db.session.commit()
 
+    # Get all members from the DB
     members = Member.query.all()
-    return render_template("index.html", title='Hello world', members=members)
+
+    # Render the template
+    return render_template("index.html", title='Index - Members', members=members)
