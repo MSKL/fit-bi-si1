b8c3830cb2039fe94cb2b7eecc5c338021784215
Matyáš Skalický <matyaskalicky@gmail.com>
2018-04-11 22:43:12 +0200

    added autoincrement

--- spravujklub/models.py
+++ spravujklub/models.py
@@ -3,6 +3,6 @@ from main import db
 
 # Temporary class
 class Member(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(50))
