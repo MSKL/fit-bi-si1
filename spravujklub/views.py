9ee6da09e373f6a3ff6ac2c4225839547f240a2e
Lukáš Langr <lucas.langr@gmail.com>
2018-04-15 16:54:24 +0200

    Added races.html and profile.html
    profile.html still work in progress

--- spravujklub/views.py
+++ spravujklub/views.py
@@ -1,6 +1,10 @@
 from flask import request, render_template
+
+from entities.Race import Race
+from entities.User import User
 from main import app, db
 from models import Member
+import datetime
 
 
 @app.route('/', methods=['GET'])
@@ -26,4 +30,20 @@ def index():
     members = Member.query.all()
 
     # Render the template
-    return render_template("index.html", members=members, title="SpravujKlub", name="Lukas")
+    return render_template("index.html", members=members, title="SpravujKlub", user=User("Lukas", "test@test.cz"))
+
+@app.route('/races', methods=['GET'])
+def races():
+    races = []
+    races.append(Race("Zavod1", "Zavodovice", datetime.date(2018, 1,8), True))
+    races.append(Race("Zavod2", "testtest", datetime.date(2018, 1,8), False))
+    races.append(Race("Zavod3", "Racetown", datetime.date(2018, 1,8), False))
+    races.append(Race("Zavod4", "Zavodnikov", datetime.date(2018, 1,8), True))
+    # Render the template
+    return render_template("races.html", races=races, user=User("Lukas", "test@test.cz"))
+
+@app.route('/profile', methods=[ 'GET' ])
+def profile():
+        # TODO
+        # Render the template
+        return render_template("profile.html", user=User("Lukas", "test@test.cz"))
