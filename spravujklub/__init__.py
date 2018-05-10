38586c141a4b3d1e979d993b8cbdb4122b551997
Matyáš Skalický <matyaskalicky@gmail.com>
2018-05-10 09:47:29 +0200

    refreshed docs

--- spravujklub/__init__.py
+++ spravujklub/__init__.py
@@ -10,16 +10,16 @@ import os
 from dl.database import db
 
 # The flask application
-templatepath = os.path.dirname(__file__) + "/pl/templates"
-app = Flask("__name__", template_folder=templatepath)
-Bootstrap(app)
+template_path = os.path.dirname(__file__) + "/pl/templates"
+app = Flask("__name__", template_folder=template_path)  #: Create a flask app
+Bootstrap(app)  #: Register the bootstrap module
 
 # Setup the _config from file
-configpath = os.path.dirname(__file__) + "/_config/config_app.py"
-app.config.from_pyfile(configpath)
+config_path = os.path.dirname(__file__) + "/_config/config_app.py"
+app.config.from_pyfile(config_path)
 
 # Setup the login manager and init
-login_manager = LoginManager()
+login_manager = LoginManager()  #: Login manager
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
@@ -27,11 +27,12 @@ login_manager.init_app(app)
 from dl.MemberController import MemberController
 from dl.RaceController import RaceController
 
-member_controller = MemberController(db)
-race_controller = RaceController(db)
+member_controller = MemberController(db)    #: DB member controller
+race_controller = RaceController(db)        #: DB race controller
 
 # Import the views
 from pl.views import *
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -57,7 +58,7 @@ def run():
     # Register the app in the database
     db.init_app(app)
 
-    # Run the application
+    #_ Run the application
     app.run(host=config_server.host, port=config_server.port)
 
 
